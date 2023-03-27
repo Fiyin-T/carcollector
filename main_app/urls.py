09c9bc8ef9e3c05f471759e66d7fb7a651e8900f
Fiyin <fiyin@MacBook.local>
2023-03-26 20:19:27 -0600

    part routes

--- main_app/urls.py
+++ main_app/urls.py
@@ -4,10 +4,20 @@ from . import views
 urlpatterns = [
     path('', views.home, name='home'),
     path('about/', views.about, name='about'),
+    # Cars URLs
     path('cars/', views.cars_index, name='index'),
     path('cars/<int:car_id>/', views.cars_detail, name='detail'),
     path('cars/create/', views.CarCreate.as_view(), name='cars_create'),
     path('cars/<int:pk>/update/', views.CarUpdate.as_view(), name='cars_update'),
     path('cars/<int:pk>/delete/', views.CarDelete.as_view(), name='cars_delete'),
-    path('cars/<int:car_id>/add_tuning/', views.add_tuning, name='add_tuning')
+    # Tuning URLs
+    path('cars/<int:car_id>/add_tuning/', views.add_tuning, name='add_tuning'),
+    # Parts URLs
+    path('parts/', views.PartList.as_view(), name='parts_index'),
+    path('parts/<int:pk>/', views.PartDetail.as_view(), name='parts_detail'),
+    path('parts/create/', views.PartCreate.as_view(), name='parts_create'),
+    path('parts/<int:pk>/update/', views.PartUpdate.as_view(), name='parts_update'),
+    path('parts/<int:pk>/delete/', views.PartDelete.as_view(), name='parts_delete'),
+    # associate a part with a car (M:M)
+    path('cars/<int:car_id>/assoc_part/<int:part_id>/', views.assoc_part, name='assoc_part')
 ]
