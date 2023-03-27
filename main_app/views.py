e0320394046817031dcc791002f93e23333e55f1
Fiyin <fiyin@MacBook.local>
2023-03-26 20:21:20 -0600

    part CBV, car detail

--- main_app/views.py
+++ main_app/views.py
@@ -1,7 +1,8 @@
 from django.shortcuts import render, redirect
-from . models import Car
+from . models import Car, Part
 from django.http import HttpResponse
 from django.views.generic.edit import CreateView, UpdateView, DeleteView
+from django.views.generic import ListView, DetailView
 from . forms import TuningForm
 
 class CarCreate(CreateView):
@@ -30,13 +31,46 @@ def cars_index(request):
 
 def cars_detail(request, car_id):
     car = Car.objects.get(id=car_id)
+    # Get parts that car doesn't have
+    parts_car_doesnt_have = Part.objects.exclude(id__in = car.parts.all().values_list('id'))
+    # instantiate TuningForm to be rendered in template
     tuning_form = TuningForm()
-    return render(request, 'cars/detail.html', { 'car': car, 'tuning_form': tuning_form })
+    return render(request, 
+        'cars/detail.html', { 
+        'car': car, 
+        'tuning_form': tuning_form, 
+        # Add parts to be displayed
+        'parts': parts_car_doesnt_have 
+        })
 
 def add_tuning(request, car_id):
+    # create ModelForm using the data in request.POST
     form = TuningForm(request.POST)
+    # validate form
     if form.is_valid():
         new_tuning = form.save(commit=False)
         new_tuning.car_id = car_id
         new_tuning.save()
+    return redirect('detail', car_id=car_id)
+
+class PartList(ListView):
+    model = Part
+
+class PartDetail(DetailView):
+    model = Part
+
+class PartCreate(CreateView):
+    model = Part
+    fields = '__all__'
+
+class PartUpdate(UpdateView):
+    model = Part
+    fields = ['name', 'color']
+
+class PartDelete(DeleteView):
+    model = Part
+    success_url = '/parts/'
+
+def assoc_part(request, car_id, part_id):
+    Car.objects.get(id=car_id).parts.add(part_id)
     return redirect('detail', car_id=car_id)
