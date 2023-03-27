c2ca18b088b9daf0236742ca57846b4b31c08d51
Fiyin <fiyin@MacBook.local>
2023-03-26 20:18:23 -0600

    updated models with part

--- main_app/models.py
+++ main_app/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 from django.urls import reverse
+from datetime import date
 
 SHOPS = (
     ('C', 'City Customs'),
@@ -8,19 +9,31 @@ SHOPS = (
 )
 
 # Create your models here.
+class Part(models.Model):
+    name = models.CharField(max_length=50)
+    color = models.CharField(max_length=20)
+
+    def __str__(self):
+        return self.name
+    
+    def get_absolute_url(self):
+        return reverse('parts_detail', kwargs={'pk': self.id})
+
 class Car(models.Model):
     make = models.CharField(max_length=100)
     model = models.CharField(max_length=100)
     year = models.IntegerField()
     description = models.TextField(max_length=250)
     age = models.IntegerField()
+    # M:M relationship
+    parts = models.ManyToManyField(Part)
 
     def __str__(self):
         return self.make
     
     def get_absolute_url(self):
         return reverse('detail', kwargs={'car_id': self.id})
-    
+  
 class Tuning(models.Model):
     date = models.DateField()
     shop = models.CharField(max_length=1,
